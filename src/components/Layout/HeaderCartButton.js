678882e3c0c7f124f33f1f762b10228c570f1736
Muhammad Gamal <moga1478@gmail.com>
2022-08-23 23:14:19 +0200

    Make button highlighting

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/Cart-Context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   console.log(cartCtx);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     // console.log(curNumber, item);
     return curNumber + item.amount;
   }, 0);
-  // console.log(numberOfCartItems);
+
+  const btnClass = `${styles.button}  ${btnHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <>
-      <button className={styles.button} onClick={props.onCartClick}>
+      <button className={btnClass} onClick={props.onCartClick}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
