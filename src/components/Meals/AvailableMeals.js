783b5ece7d395e768d21d0280939a117583d69ec
Muhammad Gamal <moga1478@gmail.com>
2022-09-01 00:28:34 +0200

    convert from useState to useReducer

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,16 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
 import MealItem from "./MealsItem/MealItem";
 
+const controllsInitialState = {
+  meals: [],
+  isLoading: true,
+  error: null,
+};
+
+const controllsReducer = (state, action) => {
+  if (action.type === "SET_MEALS") {
+    const updatedMeals = action.data;
+    return {
+      ...state,
+      meals: updatedMeals,
+    };
+  }
+  if (action.type === "SET_LOADING") {
+    const updatedLoading = action.data;
+    return {
+      ...state,
+      isLoading: updatedLoading,
+    };
+  }
+  if (action.type === "SET_ERROR") {
+    const updatedError = action.data;
+    return {
+      ...state,
+      error: updatedError,
+    };
+  }
+  return controllsInitialState;
+};
 const AvailableMeals = (props) => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsloading] = useState(true);
-  const [error, setError] = useState(null);
+  const [controlls, dispatchControlls] = useReducer(
+    controllsReducer,
+    controllsInitialState
+  );
+  // const [meals, setMeals] = useState([]);
+  // const [isLoading, setIsloading] = useState(true);
+  // const [error, setError] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        "https://order-food-e043d-default-rtdb.firebaseio.com/meal"
+        "https://order-food-e043d-default-rtdb.firebaseio.com/meal.json"
       );
       if (!response.ok) {
         throw new Error("Some thing went wrong!");
@@ -25,30 +59,38 @@ const AvailableMeals = (props) => {
           price: responseData[key].price,
         });
       }
-      setMeals(transformedMealsArray);
-      setIsloading(false);
+      dispatchControlls({ type: "SET_MEALS", data: transformedMealsArray });
+      dispatchControlls({ type: "SET_LOADING", data: false });
+      // setMeals(transformedMealsArray);
+
+      // setIsloading(false);
     };
     fetchData().catch((err) => {
-      setIsloading(false);
-      setError(err.message);
+      dispatchControlls({ type: "SET_LOADING", data: false });
+      dispatchControlls({ type: "SET_ERROR", data: err.message });
+
+      // setIsloading(false);
+      // setError(err.message);
     });
   }, []);
-  console.log(error);
-  if (isLoading) {
+  console.log(controlls);
+
+  console.log(controlls.error);
+  if (controlls.isLoading) {
     return (
       <section className={styles.mealsLoading}>
         <p>LOADING...</p>
       </section>
     );
   }
-  if (error) {
+  if (controlls.error) {
     return (
       <section className={styles.mealsError}>
-        <p>{error}</p>
+        <p>{controlls.error}</p>
       </section>
     );
   }
-  const mealsList = meals.map((item) => (
+  const mealsList = controlls.meals.map((item) => (
     <MealItem
       id={item.id}
       key={item.id}
